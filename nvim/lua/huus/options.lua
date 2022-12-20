4766aa52c57a7701e5a9e35f3788386480990a4e
Tor Einar Huus <tor@huus.me>
2022-12-20 15:07:31 +0100

    added more options

--- nvim/lua/huus/options.lua
+++ nvim/lua/huus/options.lua
@@ -1,28 +1,35 @@
 -- [[ Setting options ]]
-
 vim.o.hlsearch = false
-
 vim.wo.number = true
-
+vim.wo.relativenumber = true
 vim.o.mouse = 'a'
-
 vim.o.breakindent = true
-
 vim.o.undofile = true
-
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
+vim.o.wrap = false
+vim.o.title = true
+vim.opt.wildmode = 'longest:full,full'
 vim.o.expandtab = true
 vim.o.smartindent = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.mouse = 'a'
 vim.o.updatetime = 250
 vim.wo.signcolumn = 'yes'
-
 vim.o.termguicolors = true
 vim.cmd [[let ayucolor='dark']]
 vim.cmd [[colorscheme ayu]]
-
 vim.o.completeopt = 'menuone,noselect'
+vim.o.list = true
+vim.opt.listchars = { tab = '↦ ', trail = '▬' }
+vim.opt.fillchars:append({eob = ' '})
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.confirm = true
+vim.opt.scrolloff = 20
+vim.opt.undofile = true
+vim.opt.backup = true
+vim.opt.backupdir:remove('.')
